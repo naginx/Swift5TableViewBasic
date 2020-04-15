638702e4d59709934fb911a19364232d65ceebfd
ハナウタ三浦 <miura.hnut@gmail.com>
2020-04-15 16:06:05 +0900

    セクション11終了

--- Swift5TableViewBasic/NextViewController.swift
+++ Swift5TableViewBasic/NextViewController.swift
@@ -10,21 +10,18 @@ import UIKit
 
 class NextViewController: UIViewController {
 
+    var todoString = String()
+    @IBOutlet weak var todoLabel: UILabel!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        super.viewDidLoad()
+        todoLabel.text = todoString
     }
     
+    override func viewWillAppear(_ animated: Bool) {
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = false
     }
-    */
-
 }
--- Swift5TableViewBasic/ViewController.swift
+++ Swift5TableViewBasic/ViewController.swift
@@ -8,13 +8,77 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+
+    var textArray = [String]()
 
     override func viewDidLoad() {
+
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        textField.delegate = self
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
 
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    /// セクションの数を指定
+    func numberOfSections(in tableView: UITableView) -> Int {
 
+        return 1
+    }
+    
+    /// セクションの中のセルの数を指定
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        // 追加した文字列の数
+        return textArray.count
+    }
+    
+    /// セル構築時に呼ばれる
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        // 選択時のハイライトを消す
+        cell.selectionStyle = .none
+        // textArrayの中のテキストごとに、各セルのテキストラベルへ反映
+        cell.textLabel?.text = textArray[indexPath.row]
+        // imageViewの実行
+        cell.imageView!.image = UIImage(named: "checkImage")
+        
+        return cell
+    }
+    
+    /// セル選択時に呼ばれる
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let nextVC = storyboard?.instantiateViewController(identifier: "next") as! NextViewController
+        // nextVCにタップされた番号の文字列を渡す
+        nextVC.todoString = textArray[indexPath.row]
+        navigationController?.pushViewController(nextVC, animated: true)
+    }
+    
+    /// セルの高さを決める
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+
+        return view.frame.size.height / 6
+    }
+    
+    /// キーボードのリターン押下時に呼ばれる
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+
+        textArray.append(textField.text!)
+        textField.resignFirstResponder()
+        textField.text = ""
+        tableView.reloadData()
+        
+        return true
+    }
 }
-
