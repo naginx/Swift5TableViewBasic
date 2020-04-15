f83c187d32849db49b79515fa13bb16569e85145
ハナウタ三浦 <miura.hnut@gmail.com>
2020-04-15 16:03:47 +0900

    first commit

---
+++ Swift5TableViewBasic/NextViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NextViewController.swift
+//  Swift5TableViewBasic
+//
+//  Created by nagisa miura on 2020/04/15.
+//  Copyright © 2020 nagisa miura. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
